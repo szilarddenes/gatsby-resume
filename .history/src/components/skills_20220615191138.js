dd8ef03a6fdc858c383d5661d8680b643ce57a87
sd <szdenes@icloud.com>
2022-06-15 19:43:07 +0200

    added content 90%, new built

---
+++ .history/src/components/skills_20220615191138.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Skills = ({ skills }) => (
+  <section id='skills' className='row'>
+    <aside className='col-sm-3'>
+      <h3>Skills</h3>
+    </aside>
+    <div className='col'>
+      <div className='row'>
+        {skills.map((skill) => (
+          <div className='col-sm-9'>
+            <div className='name'>
+              <div className='flexSpan'>
+                <h4 className='strike-through'>{skill.name} <span>{skill.level}
+                </h4></span>
+              </div>
+            </div>
+            <ul className='keywords'>
+              {skill.keywords.map((keyword) => (
+                <li>{keyword}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+)
+
+export default Skills
