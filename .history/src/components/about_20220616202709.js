b29e497577354bc88a0dc16032ee203f86ef5d85
sd <szdenes@icloud.com>
2022-06-16 20:50:30 +0200

    add, highlight, fix close - #6

---
+++ .history/src/components/about_20220616202709.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = ({ basics }) => (
+  <section id="about" className="row">
+    <aside className="col-sm-3">
+      <h3>About</h3>
+    </aside>
+    <div className="col-sm-9">
+      {basics.summary.map((summ)=>(
+        <li className='bullet'>{summ}<
+      ))}
+      {/* <p>{basics.summary}</p> */}
+    </div>
+  </section>
+);
+
+export default About;
---
+++ .history/src/components/about_20220616202740.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = ({ basics }) => (
+  <section id="about" className="row">
+    <aside className="col-sm-3">
+      <h3>About</h3>
+    </aside>
+    <div className="col-sm-9">
+      <ul>
+      {basics.summary.map((summ)=>(
+        <li className='bullet'>{summ}</li>
+      ))}
+      </ul>
+      {/* <p>{basics.summary}</p> */}
+    </div>
+  </section>
+);
+
+export default About;
