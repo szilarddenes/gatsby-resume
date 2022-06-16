4559bf61e0ec4f25236bdebfa707465bf7db6364
sd <szdenes@icloud.com>
2022-06-16 22:26:15 +0200

    added content, close #13 close #18

---
+++ .history/src/components/contact_20220616220304.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Contact = ({ basics }) => (
+  <section id='contact' className='row'>
+    <aside className='col-sm-3'>
+      <h3>Contact</h3>
+    </aside>
+    <div className='col-sm-9'>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <strong>Email</strong>
+          <div className='email'>{basics.email}</div>
+        </div>
+        <div className='col-sm-6'>
+          <strong>Phone</strong>
+          <div className='phone'>{basics.phone}</div>
+        </div>
+        <div className='col-sm-6'>
+          <strong>Website</strong>
+          <div className='website'>
+            <a href={basics.url} target="_blank" >{basics.url}</a> <small>the site your view is temporary, and all my work done is stored </small>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+)
+
+export default Contact
---
+++ .history/src/pages/index_20220616210325.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Resume from '../components/resume'
+
+const Page = ({ data }) => {
+  const resume = data.resumeYaml
+  return <Resume resume={resume} />
+}
+
+export default Page
+
+export const query = graphql`
+  query MyQuery {
+    resumeYaml {
+      basics {
+        email
+        name
+        label
+        phone
+        url
+        summary
+        profiles {
+          network
+          url
+          username
+        }
+      }
+      work {
+        description
+        endDate(formatString: "MMM, YYYY")
+        highlights
+        location
+        name
+        startDate(formatString: "MMM, YYYY")
+        position
+        summary
+        url
+      }
+      volunteer {
+        endDate(formatString: "MMM, YYYY")
+        highlights
+        organization
+        position
+        startDate(formatString: "MMM, YYYY")
+        summary
+        url
+      }
+      education {
+        area
+        courses
+        endDate(formatString: "MMM, YYYY")
+        gpa
+        thesis
+        institution
+        startDate(formatString: "MMM, YYYY")
+        studyType
+      }
+      trainings{
+        title
+        location
+        heldBy
+        startDate(formatString: "DD,MMM, YYYY")
+        endDate(formatString: "DD, MMM, YYYY")
+      }
+      awards {
+        awarder
+        date(formatString: "MMM, YYYY")
+        summary
+        title
+      }
+      publications {
+        name
+        publisher
+        releaseDate(formatString: "MMM, YYYY")
+        summary
+        url
+      }
+      skills {
+        keywords
+        level
+        name
+      }
+      languages {
+        fluency
+        language
+      }
+      interests {
+        keywords
+        name
+      }
+      references {
+        name
+        reference
+        link
+      }
+      projects {
+        name
+        description
+        <Highlights>url</Highlights>
+      }
+      
+    }
+  
+  }
+`
