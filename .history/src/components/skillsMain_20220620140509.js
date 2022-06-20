8975da0124d7dbb1714f91cf1bd30949e2c0fc59
sd <szdenes@icloud.com>
2022-06-20 14:12:53 +0200

    added animation to skills section

---
+++ .history/src/components/skillsMain_20220620140509.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const SkillsMain = ({ skillsMain }) => (
+  <section id='skills' className='row'>
+    <aside className='col-sm-3'>
+      <h3>Skills</h3>
+    </aside>
+    <div className='col'>
+      <div className='row'>
+        {skillsMain.map((skill) => (
+          <div className='col-sm-12'>
+            <div className='name'>
+              <h4 className='strike-through'>
+                <span>{skill.name}</span>
+                <span className='colorTeal'>{skill.level}</span>
+              </h4>
+            </div>
+            <ul className='keywords'>
+              {skill.keywords.map((keyword) => (
+                <li>{keyword}</li>
+              ))}
+            </ul>
+            <div id='anim'>
+              <div title='html' className='anim anim1 colorYellow'>
+                html
+              </div>
+              <div title='cs' className='anim anim2 colorGreen'>
+                css
+              </div>
+              <div title='javascript' className='anim anim3 colorYellow'>
+                javascript
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+)
+
+export default SkillsMain
---
+++ .history/src/components/skills_20220619151749.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Skills = ({ skills }) => (
+  <section id='skills' className='row'>
+    <aside className='col-sm-3'>
+      <h3>Skills</h3>
+    </aside>
+    <div className='col'>
+      <div className='row'>
+        {skills.map((skill) => (
+          <div className='col-sm-12'>
+            <div className='name'>
+              <h4 className='strike-through'>
+                <span>{skill.name}</span>
+                <span className='colorTeal'>{skill.level}</span>
+              </h4>
+              <div title='html' className='anim'>html</div>
+              <div title ='cs' className='anim'>css</div>
+              <div title=' 'className='anim'>javascript</div>
+            </div>
+            <ul className='keywords'>
+              {skill.keywords.map((keyword) => (
+                <li>{keyword}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+)
+
+export default Skills
