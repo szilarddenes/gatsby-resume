61a29490bd25569b4f7fd0070e67c3ff2572a6a0
sd <szdenes@icloud.com>
2022-07-14 07:36:59 +0200

    fixing typos

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,21 +9,24 @@ const Project = ({ projects }) => (
       <div className='row'>
         {projects.map((project) => (
           <div className='col-sm-12 projectCards'>
-            <h4 className='colorGreen'>
-              {project.name}
-            </h4>
+            <h4 className='colorGreen'>{project.name}</h4>
             <div className='website pull-right'>
-              {project.url ? <a href={project.url} target="_blank">{project.url}</a> : ''}
-              
+              {project.url ? (
+                <a href={project.url} target='_blank'>
+                  {project.url}
+                </a>
+              ) : (
+                ''
+              )}
             </div>
             <div className='position'>{project.position}</div>
             <div className='summary'>
-            <p>{project.description}</p>
+              <p>{project.description}</p>
             </div>
             {project.highlights && (
               <React.Fragment>
                 <hr />
-                <h6>Highlights:</h6>
+                <h6>Highlights: </h6>
                 <ul className='highlights'>
                   {project.highlights.map((highlight) => (
                     <li className='bullet'>{highlight}</li>
